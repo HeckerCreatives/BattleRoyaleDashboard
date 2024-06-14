b47c9895e4a1ca5dc5ffe50127fc5477c277d9ce
D4rzDev <darelhonrejas86@gmail.com>
2024-06-14 16:54:04 +0800

    login bug

--- src/app/auth/Signin.tsx
+++ src/app/auth/Signin.tsx
@@ -57,7 +57,7 @@ export default function Signin() {
         })
         }
 
-        if ( response.data.message === 'success' && response.data.data.auth === 'admin') {
+        if ( response.data.message === 'success' && response.data.data.auth === 'superadmin') {
           router.push('/admin')
         setLoading(false)
         setUsername('')
@@ -65,6 +65,16 @@ export default function Signin() {
         toast({
           title: "Success",
           description: "Successfully logged in",
+        })
+        }
+
+         if ( response.data.message === 'success' && response.data.data.auth === 'player') {
+        setLoading(false)
+        setUsername('')
+        setPassword('')
+        toast({
+          title: "error",
+          description: "Unauthorized",
         })
         }
 
